07700c8b8df9d833d87f875abbdb9f12f0ffc17e
TheKost_ <thekost2@yandex.ru>
2026-01-06 20:33:18 +0300

    quizzes page layout

--- js/pages/quizzes.js
+++ js/pages/quizzes.js
@@ -1,3 +1,14 @@
 import { init } from "../utils/init";
+import {Header} from "../components/layout/header";
+import {Quiz} from "../components/quiz";
+
+import dataQuiz from "../../quiz-examples/css-quiz.json";
 
 init();
+
+const header = new Header();
+//const quizGenerator = new QuizGenerator();
+
+header.setMenuItems([
+    { text: 'Добавить квиз', href: '/', variant: 'primary' },
+]);
