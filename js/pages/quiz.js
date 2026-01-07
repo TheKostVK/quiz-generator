d2c872464b9397c793e16d51a76639144b80289d
TheKost_ <thekost2@yandex.ru>
2026-01-07 13:44:27 +0300

    quiz page layout

--- js/pages/quiz.js
+++ js/pages/quiz.js
@@ -1,14 +1,20 @@
-import { init } from "../utils/init";
+import {init} from "../utils/init";
 import {Quiz} from "../components/quiz";
 import dataQuiz from "../../quiz-examples/css-quiz.json";
+import {Header} from "../components/layout/header";
 
 init();
 
+const header = new Header();
 const quiz = new Quiz(dataQuiz);
+
+header.setMenuItems([
+    {text: 'Посмотреть сохранённые квизы', href: '/quizzes.html', variant: 'secondary'},
+]);
 
 console.log(quiz.quiz.questions);
 console.log(quiz.getNextQuestion());
 console.log(quiz.setAnswerQuestion(1, [2]));
-console.log(quiz.setAnswerQuestion(2, [1,2,3,4]));
+console.log(quiz.setAnswerQuestion(2, [1, 2, 3, 4]));
 console.log(quiz.setAnswerQuestion(3, [3]));
 console.log(quiz.getQuizResult());
