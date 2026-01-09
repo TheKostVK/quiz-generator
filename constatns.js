7814f180d64052ea64b5bc2242b072f41f2c5e82
TheKost_ <thekost2@yandex.ru>
2026-01-09 10:24:32 +0300

    readme

--- constatns.js
+++ constatns.js
@@ -3,4 +3,3 @@ export const BASE_URL = import.meta.env.BASE_URL;
 export const quizzesDBVersion = 1;
 export const quizzesDBName = 'quizzesDB';
 export const quizzesTableName = 'quizzes';
-export const processedQuizzesTableName = 'processed-quizzes';
--- js/utils/storage.js
+++ js/utils/storage.js
@@ -1,14 +1,10 @@
 import { nanoid } from 'nanoid';
 import {IndexedDBService} from "./db";
-import {processedQuizzesTableName, quizzesDBName, quizzesDBVersion, quizzesTableName} from "../../constatns";
+import {quizzesDBName, quizzesDBVersion, quizzesTableName} from "../../constatns";
 
 export const dbPromise = IndexedDBService.open(quizzesDBName, quizzesDBVersion, (db) => {
     if (!db.objectStoreNames.contains(quizzesTableName)) {
         db.createObjectStore(quizzesTableName, { keyPath: 'id' });
-    }
-
-    if (!db.objectStoreNames.contains(processedQuizzesTableName)) {
-        db.createObjectStore(processedQuizzesTableName, { keyPath: 'id' });
     }
 });
 
@@ -25,14 +21,4 @@ export async function getQuiz(id) {
 export async function getAllQuizzes() {
     const db = await dbPromise;
     return db.getAll(quizzesTableName);
-}
-
-export async function saveProcessedQuiz(quiz) {
-    const db = await dbPromise;
-    return db.put(processedQuizzesTableName, { id: nanoid(), ...quiz });
-}
-
-export async function getProcessedQuiz(id) {
-    const db = await dbPromise;
-    return db.get(processedQuizzesTableName, id);
 }
